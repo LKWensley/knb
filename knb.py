3f104ed71ceb6aaf8a4d1817b686bfa50d9d663e
LKW <45434530+LKWensley@users.noreply.github.com>
2019-03-21 19:36:56 +0400

    Можно теперь выбрать: играть или нет.

--- knb.py
+++ knb.py
@@ -1,51 +1,63 @@
 print('Камень, ножницы, бумага')
-import random
+from random import choice
 import string
 
-while True:
-    c = input('Хотите играть? Д/Н\n')
-    if c in ('д','Д'):
-        b = random.choice('кнб')
-        a = str(input('Что выберешь:\nкамень (к), ножницы (н) или бумагу (б)?\n'))
-        if a not in ('к','н','б'):
-            print('Повторите ввод.')
-        else:
-            if a == b:
-                print('Ничья.')
+while 1:
+    qstn = input('Будешь играть? (д/н)\n')
+    
+    if qstn not in ('д','Д','н','Н'):
+        print('Повторите ввод.')
+        
+    elif qstn in ('н','Н'):
+        print('До свидания!')
+        break
+    
+    elif qstn in ('д','Д'):
+        
+        while True:
+            a = input('Что выберешь:\nкамень (к), ножницы (н) или бумагу (б)?\n')
+            b = choice('кнб')
+            if a not in ('к','н','б'):
+                print('Повторите ввод.')
             else:
-                if a == ('б') and b == ('н'):
+                
+                if a == b:
+                    print('Ничья.')
+                    
+                elif a == ('б') and b == ('н'):
                     print('Я выбрал ножницы.')
                     print('Ножницы режут бумагу.')
                     print('Я победил.')
-                else:
-                    if a == ('к') and b == ('н'):
-                        print('Я выбрал ножницы.')
-                        print('Камень ломает ножницы.')
-                        print('Ты победил.')
-                    else:
-                        if a == ('к') and b == ('б'):
-                            print('Я выбрал бумагу.')
-                            print('Бумага накрыла камень.')
-                            print('Я победил.')
-                        else:
-                            if a == ('н') and b == ('б'):
-                                print('Я выбрал бумагу.')
-                                print('Ножницы режут бумагу.')
-                                print('Ты победил.')
-                            else:
-                                if a == ('н') and b == ('к'):
-                                    print('Я выбрал камень.')
-                                    print('Камень ломает ножницы.')
-                                    print('Я победил.')
-                                else:
-                                    if a == ('б') and b == ('к'):
-                                        print('Я выбрал камень.')
-                                        print('Бумага накрыла камень.')
-                                        print('Ты победил.')
+                    break
+                
+                elif a == ('к') and b == ('н'):
+                    print('Я выбрал ножницы.')
+                    print('Камень ломает ножницы.')
+                    print('Ты победил.')
+                    break
+                
+                elif a == ('к') and b == ('б'):
+                    print('Я выбрал бумагу.')
+                    print('Бумага накрыла камень.')
+                    print('Я победил.')
+                    break
+                
+                elif a == ('н') and b == ('б'):
+                    print('Я выбрал бумагу.')
+                    print('Ножницы режут бумагу.')
+                    print('Ты победил.')
+                    break
+                
+                elif a == ('н') and b == ('к'):
+                    print('Я выбрал камень.')
+                    print('Камень ломает ножницы.')
+                    print('Я победил.')
+                    break
+                
+                elif a == ('б') and b == ('к'):
+                    print('Я выбрал камень.')
+                    print('Бумага накрыла камень.')
+                    print('Ты победил.')
+                    break
 
-    elif c in ('н','Н'):
-        print('До свидания!')
-        break
 
-    else:
-        print('Повторите ввод.')
